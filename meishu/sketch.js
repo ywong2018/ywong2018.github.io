50e3711928e3ad3c61ce076ef586a83811b234c1
ywong2018 <125047337@qq.com>
2018-11-19 16:03:31 +0800

    u

--- meishu/sketch.js
+++ meishu/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
- canvastest = createCanvas(400, 400);
+ canvas = createCanvas(400, 400);
 //var canvastest = createCanvas(500, 500);
  
   // Move the canvas so it¡¯s inside our <div id="sketch-holder">.
