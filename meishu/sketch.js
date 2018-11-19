2092f645d9b4b89eb9581e72544a0c1bbeb9d6ae
ywong2018 <125047337@qq.com>
2018-11-19 16:58:20 +0800

    Update sketch.js

--- meishu/sketch.js
+++ meishu/sketch.js
@@ -5,7 +5,7 @@ function setup() {
   // Move the canvas so it¡¯s inside our <div id="sketch-holder">.
   //canvastest.parent('sketch-holder');
 
-  background(255, 0, 200);
+  background(255, 0, 200,250);
   translate(width / 2, height / 2);
   rect(0, 0, 20, 20);
 }
