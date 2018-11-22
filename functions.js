5678b91a95e3b6a60c92b053883b167f9ca3ada9
ywong2018 <125047337@qq.com>
2018-11-22 17:35:40 +0800

    u

---
+++ functions.js
@@ -0,0 +1,12 @@
+function SumNum()
+{
+var sumValue
+a=eval(shuke.value);
+b=eval(wenhua.value);
+if (isNaN(a))
+{a=0}
+if (isNaN(b))
+{b=0}
+sumValue = a*2.5*0.6+b*0.4;
+self['zonghefen'].value =sumValue;
+}
