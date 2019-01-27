3155255d7ded1b256859193d558cabe1004eaea8
ywong2018 <125047337@qq.com>
2019-01-27 14:05:39 +0800

    meishu backup 02
    美术02

---
+++ meishu02/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+ canvas = createCanvas(400, 400);
+//var canvastest = createCanvas(500, 500);
+ 
+  // Move the canvas so it¡¯s inside our <div id="sketch-holder">.
+  //canvastest.parent('sketch-holder');
+
+  background(255, 0, 200,50);
+  translate(width / 2, height / 2);
+  rect(0, 0, 20, 20);
+}
